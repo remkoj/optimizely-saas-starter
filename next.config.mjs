dc71885a66ee9d18056c2bdf5b4ae9d72fb42960
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2025-05-14 11:47:29 +0200

    Adjusted configuration to make implementation work with Optimizely Products

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            // Optimizely CMS
+            {
+                protocol: 'https',
+                hostname: '*.cms.optimizely.com',
+                pathname: '/**'
+            },
+            // Optimizely Content Recommendations
+            {
+                protocol: 'https',
+                hostname: '*.idio.co',
+                pathname: '/**'
+            },
+            // Optimizely DAM
+            {
+                protocol: 'https',
+                hostname: '*.cmp.optimizely.com',
+                pathname: '/**'
+            }
+        ]
+    },
+    experimental: {
+        serverActions: {
+          allowedOrigins: [
+            // Optimizely CMP Preview
+            '*.webproofing.cmp.optimizely.com', 
+            // Optimizely Web Experimentation & Personalization Editor
+            'www.optimizelyedit.com/' 
+          ],
+        }
+    }
+};
 
 export default nextConfig;
